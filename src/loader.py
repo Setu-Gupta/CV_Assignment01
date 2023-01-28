cf7e6798dd7c995ae615d66ef2d148810662155d
Setu Gupta <setu.gupta.2020@gmail.com>
2023-01-29 00:08:34 +0530

    Added dataset and set up git lfs
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitattributes
    	modified:   README
    	new file:   src/data/train_32x32.mat
    	modified:   src/loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,2 +1,10 @@
 import scipy.io
-mat =  scipy.io.loadmat('./data/')
+
+def get_data_XY():
+    mat =  scipy.io.loadmat('./data/train_32x32.mat')
+    X = mat['X']
+    Y = mat['y']
+    return X, Y
+
+x, y = get_data_XY()
+print(x.shape, y.shape)
