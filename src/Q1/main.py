6b0433b50ae425de3242ee2ee9f4f410dd891d7c
Setu Gupta <setu.gupta.2020@gmail.com>
2023-01-29 16:26:47 +0530

    Added code for data distribution and saving images
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   README
    	modified:   src/Q1/main.py
    	new file:   src/Q1/pictures/data_dist.png

--- src/Q1/main.py
+++ src/Q1/main.py
@@ -3,6 +3,8 @@ wandb.login()
 
 from dataset import SvnhDataset
 from torch.utils.data import DataLoader, random_split
+from matplotlib import pyplot as plt
+import numpy as np
 
 config = dict(
         train_ratio = 0.7,
@@ -22,12 +24,41 @@ def make(config):
     training_data, validation_data, testing_data = random_split(SvnhDataset(), [config['train_ratio'], config['val_ratio'], config['test_ratio']])
 
     # Create data loaders for training, validation and testing sets
-    train_loader = DataLoader(training_data, shuffle=True)
-    val_loader = DataLoader(validation_data, shuffle=True)
-    test_loader = DataLoader(testing_data, shuffle=True)
+    train_loader = DataLoader(training_data, shuffle=False)
+    val_loader = DataLoader(validation_data, shuffle=False)
+    test_loader = DataLoader(testing_data, shuffle=False)
 
-    # TODO
     # Visualize the data distribution
+    figure, axes = plt.subplots(nrows=1, ncols=3, figsize=(20, 5))
+    
+    train_targets = labels = [l.item() for _, l in train_loader]
+    train_ticks = np.unique(train_targets)
+    train_ticks.sort()
+    axes[0].hist(train_targets)
+    axes[0].set_xticks(train_ticks)
+    axes[0].set_title("Class-Wise Data Distribution [Train]")
+    axes[0].set_xlabel("Target Labels")
+    axes[0].set_ylabel("Count")
+    
+    val_targets = labels = [l.item() for _, l in val_loader]
+    val_ticks = np.unique(val_targets)
+    val_ticks.sort()
+    axes[1].hist(val_targets)
+    axes[1].set_xticks(val_ticks)
+    axes[1].set_title("Class-Wise Data Distribution [Validation]")
+    axes[1].set_xlabel("Target Labels")
+    axes[1].set_ylabel("Count")
+    
+    test_targets = labels = [l.item() for _, l in test_loader]
+    test_ticks = np.unique(test_targets)
+    test_ticks.sort()
+    axes[2].hist(test_targets)
+    axes[2].set_xticks(test_ticks)
+    axes[2].set_title("Class-Wise Data Distribution [Test]")
+    axes[2].set_xlabel("Target Labels")
+    axes[2].set_ylabel("Count")
+
+    plt.savefig("./pictures/data_dist.png")
 
     return model, train_loader, val_loader, test_loader
 
